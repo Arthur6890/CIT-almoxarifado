a69d4186d79639297a5b03c15c47a599376125e3
arthur6890 <arthur.ramos.amaral@hotmail.com>
2026-09-10 13:22:36 -0300

    correcao final de tipos

--- src/hooks/useItems.ts
+++ src/hooks/useItems.ts
@@ -1,11 +1,22 @@
 // src/hooks/useItems.ts
 import { useLiveQuery } from "dexie-react-hooks";
 import { db } from "../lib/db";
-import { buscarItensPorTermo, listarHistorico, listarUltimasMovimentacoes } from "../lib/repository";
-import type { HistoricoFiltros, Item, MovimentacaoComItem, Setor } from "../lib/types";
+import {
+  buscarItensPorTermo,
+  listarHistorico,
+  listarUltimasMovimentacoes,
+} from "../lib/repository";
+import type {
+  HistoricoFiltros,
+  Item,
+  MovimentacaoComItem,
+  Projeto,
+} from "../lib/types";
 
-export function useSetores(): Setor[] {
-  return useLiveQuery(() => db.setores.orderBy("nome").toArray(), [], []) ?? [];
+export function useSetores(): Projeto[] {
+  return (
+    useLiveQuery(() => db.projetos.orderBy("nome").toArray(), [], []) ?? []
+  );
 }
 
 export function useItensPorSetor(setorId: number | "", termo: string): Item[] {
@@ -22,7 +33,9 @@ export function useItensPorSetor(setorId: number | "", termo: string): Item[] {
 }
 
 export function useUltimasMovimentacoes(limite = 5): MovimentacaoComItem[] {
-  return useLiveQuery(() => listarUltimasMovimentacoes(limite), [limite], []) ?? [];
+  return (
+    useLiveQuery(() => listarUltimasMovimentacoes(limite), [limite], []) ?? []
+  );
 }
 
 export function useHistorico(filtros: HistoricoFiltros): MovimentacaoComItem[] {
